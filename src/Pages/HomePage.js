cd54d47635f4a2515e24a6d6ebe4ae9f48e593b0
soheilmansoori <soheilmansoori@gmail.com>
2023-09-04 20:47:03 +0330

    add banner

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -3,6 +3,7 @@ import Offer from '../componemts/Amazing offer/Offer'
 import Category from '../componemts/Category/Category'
 import Slider from '../componemts/slider/Slider'
 import BestSellingProducts from '../componemts/Best selling products/BestSellingProducts'
+import MiddleBanner from '../componemts/banners/MiddleBanner'
 function HomePage() {
   return (
   <>
@@ -10,6 +11,7 @@ function HomePage() {
   <Category />
   <Offer />
   <BestSellingProducts />
+  <MiddleBanner />
   </>      
   )
 }
---
+++ src/componemts/banners/MiddleBanner.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Container from '@mui/material/Container'
+
+function MiddleBanner() {
+  return (
+    <section>
+        <Container maxWidth="xl">
+            <img className="rounded-2xl" src="./imgs/Banners/banner-1.jpg" alt=""></img>
+        </Container>
+    </section>
+  )
+}
+
+export default MiddleBanner
